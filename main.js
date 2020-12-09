b4f38b8bee8815fa1193aac6d76ca3d10b641341
tsuen4 <ht17a045@oecu.jp>
2020-12-09 16:01:01 +0900

    uuidv4 なディレクトリ名でディレクトリを作成するように

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
+const fs = require('fs')
 const { join } = require('path')
 const express = require('express')
 const app = express()
 const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const { v4: uuidv4 } = require('uuid')
+
+const mkdir_uuidv4 = (baseDirectory) => {
+  // 名前が衝突せずにディレクトリの作成ができたらディレクトリ名を返す
+  while (true) {
+    const directory = `${baseDirectory}/${uuidv4()}`
+    if (!fs.existsSync(directory)) {
+      fs.mkdirSync(directory)
+      return directory
+    }
+    // console.log(`ディレクトリ ${directory} を作成できません`)
+  }
+}
+
+const storage = multer.diskStorage({
+  destination (req, file, cb) {
+    const directory = mkdir_uuidv4('uploads')
+    cb(null, directory)
+  },
+  filename (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+const upload = multer({ storage: storage })
 
 app.set('port', process.env.PORT || 3000)
 app.listen(app.get('port'), () => {
