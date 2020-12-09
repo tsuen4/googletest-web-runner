77771b6c5b8d158642452cfc79e200f1a455996b
tsuen4 <etsushigs3052@gmail.com>
2020-12-09 14:11:48 +0900

    init

---
+++ main.js
@@ -0,0 +1,23 @@
+const { join } = require('path')
+const express = require('express')
+const app = express()
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+app.set('port', process.env.PORT || 3000)
+app.listen(app.get('port'), () => {
+  console.log(`http://localhost:${app.get('port')}`)
+})
+
+app.get('/', (req, res) => {
+  res.send('foo bar')
+})
+
+app.get('/upload', (req, res) => res.sendFile(join(__dirname, 'public/upload.html')))
+
+app.post('/upload', upload.single('code'), (req, res) => {
+  res.send('uppi!')
+})
+// app.post('/upload', upload.array('code'), (req, res) => {
+//   res.send('uppi!')
+// })
