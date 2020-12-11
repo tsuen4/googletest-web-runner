de61f7a75b0f3ce40d15d3bf3413856f24c99c07
tsuen4 <ht17a045@oecu.jp>
2020-12-11 20:53:38 +0900

    ファイルサイズが大きかった場合のエラーハンドリングを追加

--- main.js
+++ main.js
@@ -44,25 +44,49 @@ app.get('/uuid', (req, res) => {
 
 app.get('/', (req, res) => res.sendFile(join(__dirname, 'public/upload.html')))
 
-app.post('/run', upload.array('codes'), (req, res) => {
-  // multer.diskStorage にも req に対しての処理がある
-  const id = req.body.uuidv4
-  const runDirectory = join(__dirname, 'uploads', id)
+app.post('/run', (req, res) => {
+  upload.array('codes')(req, res, (err) => {
+    // multer.diskStorage にも req に対しての処理がある
+    const id = req.body.uuidv4
+    const runDirectory = join(__dirname, 'uploads', id)
 
-  // ファイル名のみを docker の引数に渡すように
-  const regId = new RegExp('uploads/' + id + '/')
-  const files = req.files.map(value => value.path.replace(regId, ''))
-  // console.log(files)
+    if (err instanceof multer.MulterError) {
+      console.dir(err)
 
-  const argFiles = files.join(' ')
-  const runCommand = `run -v ${runDirectory}:/gtest --rm gtest_runner ${argFiles}`
-  // console.log(runCommand)
+      if (err.code === 'LIMIT_FILE_SIZE') {
+        res.send('ファイルサイズが大きいです！')
+      } else {
+        res.status(500)
+        res.send(err)
+      }
+      rmdir(runDirectory)
 
-  execFile('docker', runCommand.split(' '), (err, stdout, stderr) => {
-    const response = stdout + stderr
-    console.log(response)
-    res.send(response)
-    rmdir(runDirectory)
+      return
+    } else if (err) {
+      console.error(err)
+
+      res.status(500)
+      res.end()
+      rmdir(runDirectory)
+
+      return
+    }
+
+    // ファイル名のみを docker の引数に渡すように
+    const regId = new RegExp('uploads/' + id + '/')
+    const files = req.files.map(value => value.path.replace(regId, ''))
+    // console.log(files)
+
+    const argFiles = files.join(' ')
+    const runCommand = `run -v ${runDirectory}:/gtest --rm gtest_runner ${argFiles}`
+    // console.log(runCommand)
+
+    execFile('docker', runCommand.split(' '), (err, stdout, stderr) => {
+      const response = stdout + stderr
+      console.log(response)
+      res.send(response)
+      rmdir(runDirectory)
+    })
   })
 })
 // app.post('/upload', upload.array('code'), (req, res) => {
