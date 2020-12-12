0d47a7d54f8044c10ad49eea2dcf4b92c73266b6
tsuen4 <ht17a045@oecu.jp>
2020-12-12 17:44:36 +0900

    mimetype または 拡張子が違う場合、エラーを返すように

--- main.js
+++ main.js
@@ -24,6 +24,20 @@ const upload = multer({
   storage: storage,
   limits: {
     fileSize: 10 * 1000 // 10 KB
+  },
+  fileFilter (req, file, cb) {
+    const fileExtention = /(cpp|h|hpp)$/
+    // console.log(file)
+    if (file.mimetype !== 'text/plain') {
+      const message = '不正なファイルです。'
+      // console.log(message)
+      cb(new Error(message))
+    } else if (!fileExtention.test(file.originalname)) {
+      const message = '不正なファイルです。'
+      // console.log(message)
+      cb(new Error(message))
+    }
+    cb(null, true)
   }
 })
 
