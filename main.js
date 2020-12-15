d4abbac4fae7a5c5d920de719f6860fdbaa1f407
tsuen4 <ht17a045@oecu.jp>
2020-12-15 14:50:43 +0900

    使用するポートを 3000 に設定

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const routes = require('./routes/router')
 const express = require('express')
 const app = express()
 
-app.set('port', process.env.PORT || 3002)
+app.set('port', process.env.PORT || 3000)
 app.use('/', routes)
 app.listen(app.get('port'), () => {
   console.log(`http://localhost:${app.get('port')}`)
